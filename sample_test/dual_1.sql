48048c9ddf1c05a08717d77e8de7dcb80d331970
Paweł Wieczorek <52973510+bipawelwieczorek@users.noreply.github.com>
2024-10-18 14:25:04 +0200

    Create dual_1.sql

---
+++ sample_test/dual_1.sql
@@ -0,0 +1,12 @@
+/**
+ * this is sample test
+ */
+select 1/3 AS "DIFF col"
+,2 as diff_col_2
+, current_date 
+, current_timestamp
+, 1/3 AS a_col
+, 7/8 AS b_col
+,NULL as NULL_
+/* some comment */
+from dual
